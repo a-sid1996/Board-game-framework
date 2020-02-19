6a9a03d922a922dc4cbfe619af8d324355a39d35
jatangohel <jatangohel@gmail.com>
2020-02-19 17:18:06 -0500

    added Coding Standard doc

---
+++ test/PlayerTest.java
@@ -0,0 +1,13 @@
+import model.Tile;
+import model.Player;
+
+public class PlayerTest {
+    private String name = "test name";
+    private Tile currentTile;
+
+    @org.junit.Test
+    public void updateCurrentTile() {
+        Player pm = new Player(name);
+    }
+
+}
