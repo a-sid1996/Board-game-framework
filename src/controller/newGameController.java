4699abfd94c945fd4763bfd6fc65ca146279f849
Siddhant Arora <siddhantarora1996@gmail.com>
2020-03-28 10:47:04 -0400

    NewGame fxml added

--- src/controller/newGameController.java
+++ src/controller/newGameController.java
@@ -1,5 +1,37 @@
 package controller;
 
+import java.io.File;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+
 public class newGameController {
 
+	ObservableList<String> number = FXCollections.observableArrayList("2","3","4","5");
+	
+	@FXML
+	private TextField mapLocation;
+
+
+	@FXML
+	void findFileClick(ActionEvent event) throws NumberFormatException, IOException {
+		FileChooser fc = new FileChooser();
+		fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON file", "*.json"));
+		File file = fc.showOpenDialog(null);
+		
+		if(file != null) {
+			mapLocation.setText(file.getAbsolutePath());
+		}
+		
+		
+
+	}
+		
+	
+	
 }
