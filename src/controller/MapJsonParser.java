1135a2711dd60d08963e3c4e3a392a2b6042e86e
krunal Jagani <krunaljagani7996@gmail.com>
2020-04-11 14:45:23 -0400

    updated INvalidMapException | mapJsonParser

--- src/controller/MapJsonParser.java
+++ src/controller/MapJsonParser.java
@@ -42,182 +42,221 @@ public class MapJsonParser {
 		for (int i = 0, size = jsonArray.length(); i < size; i++) {
 			JSONObject objectInArray = jsonArray.getJSONObject(i);
 			String[] elementNames = JSONObject.getNames(objectInArray);
-
 			TreeMap<String, Object> tmap = new TreeMap<String, Object>();
+			
 			for (String elementName : elementNames) {
 				String value = objectInArray.getString(elementName);
-				tmap.put(elementName, value);
-			}
-			
-			Tile tile = null;
-			
-			
-			if (tmap.get("b_type").equals("go")) {
-
-				String name = (String) tmap.get("a_name");
-				int x = Integer.parseInt((String) tmap.get("c_x"));
-				int y = Integer.parseInt((String) tmap.get("c_x"));
-				tile = new Tile(name, x, y);
-				tile.setTypeOfTile("go");
-				TileList.add(tile);
-
-			}
-
-			else if (tmap.get("b_type").equals("property")) {
-
-				String name = (String) tmap.get("a_name");
-				int x = Integer.parseInt((String) tmap.get("c_x"));
-				int y = Integer.parseInt((String) tmap.get("c_x"));
-				int cost = Integer.parseInt((String) tmap.get("e_cost"));
-				int house = Integer.parseInt((String) tmap.get("g_house"));
-				String tempRent = (String) tmap.get("f_rent");
-				tempRent = tempRent.replace("[", "");
-				tempRent = tempRent.replace("]", "");
-				String vals[] = tempRent.split(",");
-				ArrayList<Integer> rentList = new ArrayList<Integer>();
 				
-				for(String s: vals) 
+				if(value != null) 
 				{
-					rentList.add(Integer.parseInt(s));
+					if(elementName.equalsIgnoreCase("a_name") && value instanceof String) 
+					{
+						tmap.put(elementName, value);
+					}
+					
+					if(elementName.equalsIgnoreCase("b_type") && value instanceof String)
+					{
+						tmap.put(elementName, value);
+					}
+					
+					if(elementName.equalsIgnoreCase("c_x") && Float.parseFloat(value)% 1==0 && Float.parseFloat(value) >=0 ) 
+					{
+						tmap.put(elementName, value);
+					}
+					
+					if(elementName.equalsIgnoreCase("d_y") && Float.parseFloat(value)% 1==0 && Float.parseFloat(value) >=0 ) 
+					{
+						tmap.put(elementName, value);
+					}
+					
+					if(elementName.equalsIgnoreCase("e_cost") && Float.parseFloat(value)% 1==0 && Float.parseFloat(value) >0 ) 
+					{
+						tmap.put(elementName, value);
+					}
+					if(elementName.equalsIgnoreCase("g_house") && Float.parseFloat(value)% 1==0 && Float.parseFloat(value) >0 ) 
+					{
+						tmap.put(elementName, value);
+					}
+					if(elementName.equalsIgnoreCase("e_cost") && Float.parseFloat(value)% 1==0 && Float.parseFloat(value) >0 ) 
+					{
+						tmap.put(elementName, value);
+					}
+					
+					
 				}
 				
-				tile = new Tile(name, x, y);	
-				tile.setTypeOfTile("property");
-				tile.setValue("Cost", cost);
-				tile.setValue("house", house);
-				tile.setValue("rent1", rentList.get(0));
-				tile.setValue("rent2", rentList.get(1));
-				tile.setValue("rent3", rentList.get(2));
-				tile.setValue("rent4", rentList.get(3));
-				tile.setValue("rent5", rentList.get(4));
-				tile.setValue("rent6", rentList.get(5));
-				TileList.add(tile);
-
 			}
-
-			else if (tmap.get("b_type").equals("communit-chest")) {
-
-				String name = (String) tmap.get("a_name");
-				int x = Integer.parseInt((String) tmap.get("c_x"));
-				int y = Integer.parseInt((String) tmap.get("c_x"));
-				tile = new Tile(name, x, y);
-				tile.setTypeOfTile("communit-chest");
-				TileList.add(tile);
-
-			}
-
-			else if (tmap.get("b_type").equals("tax")) {
-
-				String name = (String) tmap.get("a_name");
-				int x = Integer.parseInt((String) tmap.get("c_x"));
-				int y = Integer.parseInt((String) tmap.get("c_x"));
-				int cost = Integer.parseInt((String) tmap.get("e_cost"));
-				tile = new Tile(name, x, y);	
-				tile.setTypeOfTile("tax");
-				TileList.add(tile);
-
-			}
-
-			else if (tmap.get("b_type").equals("railroad")) {
-
-				String name = (String) tmap.get("a_name");
-				int x = Integer.parseInt((String) tmap.get("c_x"));
-				int y = Integer.parseInt((String) tmap.get("c_x"));
-				int cost = Integer.parseInt((String) tmap.get("e_cost"));
-				tile = new Tile(name, x, y);
-				tile.setTypeOfTile("railroad");
-				tile.setValue("Cost", cost);
-				TileList.add(tile);
-
-			}
-
-			else if (tmap.get("b_type").equals("chance")) {
-
-				String name = (String) tmap.get("a_name");
-				int x = Integer.parseInt((String) tmap.get("c_x"));
-				int y = Integer.parseInt((String) tmap.get("c_x"));
-				tile = new Tile(name, x, y);
-				tile.setTypeOfTile("chance");
-				TileList.add(tile);
-
-			}
-
-			else if (tmap.get("b_type").equals("go-to-jail")) {
-
-				String name = (String) tmap.get("a_name");
-				int x = Integer.parseInt((String) tmap.get("c_x"));
-				int y = Integer.parseInt((String) tmap.get("c_x"));
-				tile = new Tile(name, x, y);
-				tile.setTypeOfTile("go-to-jail");
-				TileList.add(tile);
-
-			}
-
-			else if (tmap.get("b_type").equals("utility")) {
-
-				String name = (String) tmap.get("a_name");
-				int x = Integer.parseInt((String) tmap.get("c_x"));
-				int y = Integer.parseInt((String) tmap.get("c_x"));
-				int cost = Integer.parseInt((String) tmap.get("e_cost"));
-				tile = new Tile(name, x, y);
-				tile.setTypeOfTile("utility");
-				tile.setValue("Cost", cost);
-				TileList.add(tile);
-
-			}
-      
-			else if (tmap.get("b_type").equals("free-parking")) {
-
-				String name = (String) tmap.get("a_name");
-				int x = Integer.parseInt((String) tmap.get("c_x"));
-				int y = Integer.parseInt((String) tmap.get("c_x"));
-				tile = new Tile(name, x, y);
-				tile.setTypeOfTile("free-parking");
-				TileList.add(tile);
-
-			}	
-
-		}
-		System.out.println("Total tile objects created :  " +TileList.size());
-		
-		for(Tile tile : TileList) 
-		{
 			
-			if(tile.getTileName().equalsIgnoreCase("Baltic Avenue")) 
-			{
-				System.out.println("before tile edit");
-				System.out.println(tile.getTileName());
-				System.out.println(tile.getInternalValue());
-							
-			}			
+			System.out.println(tmap);
+//			Tile tile = null;
+//			
+//			
+//			if (tmap.get("b_type").equals("go")) {
+//
+//				String name = (String) tmap.get("a_name");
+//				int x = Integer.parseInt((String) tmap.get("c_x"));
+//				int y = Integer.parseInt((String) tmap.get("c_x"));
+//				tile = new Tile(name, x, y);
+//				tile.setTypeOfTile("go");
+//				TileList.add(tile);
+//
+//			}
+//
+//			else if (tmap.get("b_type").equals("property")) {
+//
+//				String name = (String) tmap.get("a_name");
+//				int x = Integer.parseInt((String) tmap.get("c_x"));
+//				int y = Integer.parseInt((String) tmap.get("c_x"));
+//				int cost = Integer.parseInt((String) tmap.get("e_cost"));
+//				int house = Integer.parseInt((String) tmap.get("g_house"));
+//				String tempRent = (String) tmap.get("f_rent");
+//				tempRent = tempRent.replace("[", "");
+//				tempRent = tempRent.replace("]", "");
+//				String vals[] = tempRent.split(",");
+//				ArrayList<Integer> rentList = new ArrayList<Integer>();
+//				
+//				for(String s: vals) 
+//				{
+//					rentList.add(Integer.parseInt(s));
+//				}
+//				
+//				tile = new Tile(name, x, y);	
+//				tile.setTypeOfTile("property");
+//				tile.setValue("Cost", cost);
+//				tile.setValue("house", house);
+//				tile.setValue("rent1", rentList.get(0));
+//				tile.setValue("rent2", rentList.get(1));
+//				tile.setValue("rent3", rentList.get(2));
+//				tile.setValue("rent4", rentList.get(3));
+//				tile.setValue("rent5", rentList.get(4));
+//				tile.setValue("rent6", rentList.get(5));
+//				TileList.add(tile);
+//
+//			}
+//
+//			else if (tmap.get("b_type").equals("communit-chest")) {
+//
+//				String name = (String) tmap.get("a_name");
+//				int x = Integer.parseInt((String) tmap.get("c_x"));
+//				int y = Integer.parseInt((String) tmap.get("c_x"));
+//				tile = new Tile(name, x, y);
+//				tile.setTypeOfTile("communit-chest");
+//				TileList.add(tile);
+//
+//			}
+//
+//			else if (tmap.get("b_type").equals("tax")) {
+//
+//				String name = (String) tmap.get("a_name");
+//				int x = Integer.parseInt((String) tmap.get("c_x"));
+//				int y = Integer.parseInt((String) tmap.get("c_x"));
+//				int cost = Integer.parseInt((String) tmap.get("e_cost"));
+//				tile = new Tile(name, x, y);	
+//				tile.setTypeOfTile("tax");
+//				TileList.add(tile);
+//
+//			}
+//
+//			else if (tmap.get("b_type").equals("railroad")) {
+//
+//				String name = (String) tmap.get("a_name");
+//				int x = Integer.parseInt((String) tmap.get("c_x"));
+//				int y = Integer.parseInt((String) tmap.get("c_x"));
+//				int cost = Integer.parseInt((String) tmap.get("e_cost"));
+//				tile = new Tile(name, x, y);
+//				tile.setTypeOfTile("railroad");
+//				tile.setValue("Cost", cost);
+//				TileList.add(tile);
+//
+//			}
+//
+//			else if (tmap.get("b_type").equals("chance")) {
+//
+//				String name = (String) tmap.get("a_name");
+//				int x = Integer.parseInt((String) tmap.get("c_x"));
+//				int y = Integer.parseInt((String) tmap.get("c_x"));
+//				tile = new Tile(name, x, y);
+//				tile.setTypeOfTile("chance");
+//				TileList.add(tile);
+//
+//			}
+//
+//			else if (tmap.get("b_type").equals("go-to-jail")) {
+//
+//				String name = (String) tmap.get("a_name");
+//				int x = Integer.parseInt((String) tmap.get("c_x"));
+//				int y = Integer.parseInt((String) tmap.get("c_x"));
+//				tile = new Tile(name, x, y);
+//				tile.setTypeOfTile("go-to-jail");
+//				TileList.add(tile);
+//
+//			}
+//
+//			else if (tmap.get("b_type").equals("utility")) {
+//
+//				String name = (String) tmap.get("a_name");
+//				int x = Integer.parseInt((String) tmap.get("c_x"));
+//				int y = Integer.parseInt((String) tmap.get("c_x"));
+//				int cost = Integer.parseInt((String) tmap.get("e_cost"));
+//				tile = new Tile(name, x, y);
+//				tile.setTypeOfTile("utility");
+//				tile.setValue("Cost", cost);
+//				TileList.add(tile);
+//
+//			}
+//      
+//			else if (tmap.get("b_type").equals("free-parking")) {
+//
+//				String name = (String) tmap.get("a_name");
+//				int x = Integer.parseInt((String) tmap.get("c_x"));
+//				int y = Integer.parseInt((String) tmap.get("c_x"));
+//				tile = new Tile(name, x, y);
+//				tile.setTypeOfTile("free-parking");
+//				TileList.add(tile);
+//
+//			}	
+//
+//		}
+//		System.out.println("Total tile objects created :  " +TileList.size());
+//		
+//		for(Tile tile : TileList) 
+//		{
+//			
+//			if(tile.getTileName().equalsIgnoreCase("Baltic Avenue")) 
+//			{
+//				System.out.println("before tile edit");
+//				System.out.println(tile.getTileName());
+//				System.out.println(tile.getInternalValue());
+//							
+//			}			
+//		}
+//	
+//		
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter name to edit : ");
+//		String nametoedit  = sc.nextLine();
+//		System.out.println("Enter cost  value to edit : ");
+//		int costtoEdit = Integer.parseInt(sc.nextLine());
+//		System.out.println("Enter house value to edit : ");
+//		int housetoEdit = Integer.parseInt(sc.nextLine());
+//	
+//		EditMap.EditAvenueCost(nametoedit, costtoEdit);
+//		EditMap.EditAvenuehouse(nametoedit, housetoEdit);
+//		
+//		for(Tile tile : TileList) 
+//		{
+//			
+//			if(tile.getTileName().equalsIgnoreCase(nametoedit)) 
+//			{
+//				System.out.println("After tile edit");
+//				System.out.println(tile.getTileName());
+//				System.out.println(tile.getInternalValue());
+////				System.out.println(tile.getValue("cost"));
+////				System.out.println(tile.getValue("house"));
+//							
+//			}			
 		}
-	
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter name to edit : ");
-		String nametoedit  = sc.nextLine();
-		System.out.println("Enter cost  value to edit : ");
-		int costtoEdit = Integer.parseInt(sc.nextLine());
-		System.out.println("Enter house value to edit : ");
-		int housetoEdit = Integer.parseInt(sc.nextLine());
-	
-		EditMap.EditAvenueCost(nametoedit, costtoEdit);
-		EditMap.EditAvenuehouse(nametoedit, housetoEdit);
-		
-		for(Tile tile : TileList) 
-		{
-			
-			if(tile.getTileName().equalsIgnoreCase(nametoedit)) 
-			{
-				System.out.println("After tile edit");
-				System.out.println(tile.getTileName());
-				System.out.println(tile.getInternalValue());
-//				System.out.println(tile.getValue("cost"));
-//				System.out.println(tile.getValue("house"));
-							
-			}			
-		}
-	
+//	
 		
 		
 	}
